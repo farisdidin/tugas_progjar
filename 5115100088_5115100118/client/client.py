e1bd047c7891be2711e392d4c51fef6e405a1dd3
farisdidin <dinzen2304@gmail.com>
2017-10-01 16:13:56 +0700

    server dan client

--- 5115100088_5115100118/client/client.py
+++ 5115100088_5115100118/client/client.py
@@ -6,3 +6,12 @@ server_address = ('127.0.0.1', 5000)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
+
+try:
+	while True:
+		message = sys.stdin.readline()
+		client_socket.send(message)
+		
+except KeyboardInterrupt:
+	client_socket.close()
+	sys.exit(0)
